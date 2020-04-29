03fa244744b369dd8179cb8a89df07e1e69072f2
Emilio Garcia <egarcia@redhat.com>
2020-04-29 10:53:42 -0400

    Use go mod
    Co-authored-by: Mike Fedosin <mfedosin@redhat.com>

---
+++ tools.go
@@ -0,0 +1,12 @@
+// +build tools
+
+// Official workaround to track tool dependencies with go modules:
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
+package tools
+
+import (
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/openshift/machine-api-operator/cmd/machineset"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+)
