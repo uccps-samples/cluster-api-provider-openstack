uccps-samples/cluster-api-provider-openstack

commit 855a22c09317457ecfd08544afac6948cfdfe462

OCPBUGS-1765: Apply noAllowedAddressPairs on intended subnets only
Before this change, setting `noAllowedAddressPairs` on a machine-pool
network could have effect on a different network.

Given this example configuration:

```yaml
networks:
- filter: {}
  noAllowedAddressPairs: false
  subnets:
  - filter: {}
    uuid: primary-subnet-uuid
- filter: {}
  noAllowedAddressPairs: true
  subnets:
  - filter: {}
    uuid: other-subnet-uuid
primarySubnet: primary-subnet-uuid
```

The filter of the second network the array is empty. This means that its
`subnet` filter has to be applied without restrictions as to which
network it's sitting on. However, the absence of a network filter also
meant that the setting `noAllowedAddressPairs` would apply to all
networks.

With this change, `noAllowedAddressPairs` is applied on a subnet basis,
meaning that only ports created in the subnets resulting from the
further `subnet` filter actually have their allowed address pairs
removed.

pkg/cloud/openstack/clients/machineservice.go
@@ -569,7 +569,7 @@ func (is *InstanceService) InstanceCreate(clusterName string, name string, clust
 	}
 	// Get all network UUIDs
 	var nets []openstackconfigv1.PortOpts
-	netsWithoutAllowedAddressPairs := map[string]struct{}{}
+	subnetsWithoutAllowedAddressPairs := map[string]struct{}{}
 	for _, net := range config.Networks {
 		opts := networks.ListOpts(net.Filter)
 		opts.ID = net.UUID
@@ -578,9 +578,6 @@ func (is *InstanceService) InstanceCreate(clusterName string, name string, clust
 			return nil, err
 		}
 		for _, netID := range ids {
-			if net.NoAllowedAddressPairs {
-				netsWithoutAllowedAddressPairs[netID] = struct{}{}
-			}
 			if net.Subnets == nil {
 				nets = append(nets, openstackconfigv1.PortOpts{
 					NetworkID:    netID,
@@ -612,6 +609,9 @@ func (is *InstanceService) InstanceCreate(clusterName string, name string, clust
 					// See https://bugzilla.redhat.com/show_bug.cgi?id=2033862
 					if snet.NetworkID != netID {
 						continue
+					}
+					if net.NoAllowedAddressPairs {
+						subnetsWithoutAllowedAddressPairs[snet.ID] = struct{}{}
 					}
 					nets = append(nets, openstackconfigv1.PortOpts{
 						NetworkID:    snet.NetworkID,
@@ -655,7 +655,7 @@ func (is *InstanceService) InstanceCreate(clusterName string, name string, clust
 		}
 		portOpt.SecurityGroups = &securityGroups
 		portOpt.AllowedAddressPairs = allowedAddressPairs
-		if _, ok := netsWithoutAllowedAddressPairs[portOpt.NetworkID]; ok {
+		if _, ok := subnetsWithoutAllowedAddressPairs[portOpt.NameSuffix]; ok {
 			portOpt.AllowedAddressPairs = []openstackconfigv1.AddressPair{}
 		}
 

commit 1376e5aca2896fc967c29c2b62a5e96f6043d927

OCPBUGS-2508: Ensure network defs without subnet follow noAllowedAddressPairs
Commit 855a22c09317457ecfd08544afac6948cfdfe462 introduced a bug that
caused network definitions without subnets (the one you would get when
setting `additionalNetworkIDs` in your `install-config.yaml`) caused the
noAllowedAddressPairs property to be skipped while creating the port.

This may in turn caused machine-api to fail creating the port because it
does not have the permission to create a port with allowed_address_pair,
which is often the case when using provider networks.

pkg/cloud/openstack/clients/machineservice.go
@@ -587,6 +587,9 @@ func (is *InstanceService) InstanceCreate(clusterName string, name string, clust
 					Profile:      net.Profile,
 					PortSecurity: net.PortSecurity,
 				})
+				if net.NoAllowedAddressPairs {
+					subnetsWithoutAllowedAddressPairs[net.UUID] = struct{}{}
+				}
 			}
 
 			for _, snetParam := range net.Subnets {